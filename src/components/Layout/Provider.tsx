f5ad35baacd6ceb51cf37d6f5eafc511a5961944
Marko Ticinovic <markoticinovic25@gmail.com>
2025-10-29 03:53:23 +0100

    first commit

---
+++ src/components/Layout/Provider.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { useState } from 'react';
+
+const Providers = ({ children }: { children: React.ReactNode }) => {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 1000 * 60 * 5, // 5 minutes
+          },
+        },
+      }),
+  );
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
+
+export default Providers;
